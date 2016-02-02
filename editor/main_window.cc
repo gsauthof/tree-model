e5ceccee1eb9cd558afc597788a7d9c7fb23873b
Georg Sauthoff <mail@georg.so>
2016-02-02 09:07:00 +0100

    rename actions

--- editor/main_window.cc
+++ editor/main_window.cc
@@ -33,11 +33,11 @@ namespace editor {
   {
     ui->setupUi(this);
     recent_menu_ = new Recent_Menu(ui->menu_File);
-    ui->menu_File->insertMenu(ui->action_Save, recent_menu_);
+    ui->menu_File->insertMenu(ui->save_action, recent_menu_);
     setup_shortcuts();
     setWindowTitle(tr("unnamed[*]"));
 
-    connect(ui->action_About, &QAction::triggered,
+    connect(ui->about_action, &QAction::triggered,
         [this](){
           QMessageBox::about(this, tr("About..."),
               tr("Hierarchical tree model editor example."));
@@ -45,9 +45,9 @@ namespace editor {
 
     // updates the window title iff it contains the [*] placeholder
 
-    ui->action_copy->setEnabled(false);
+    ui->copy_action->setEnabled(false);
     connect(ui->widget, &Tree_Widget::something_selected,
-        ui->action_copy, &QAction::setEnabled);
+        ui->copy_action, &QAction::setEnabled);
 
   }
 
@@ -61,31 +61,31 @@ namespace editor {
   }
   QAction &Main_Window::open_action()
   {
-    return *ui->action_Open;
+    return *ui->open_action;
   }
   QAction &Main_Window::save_action()
   {
-    return *ui->action_Save;
+    return *ui->save_action;
   }
   QAction &Main_Window::save_as_action()
   {
-    return *ui->action_Save_As;
+    return *ui->save_as_action;
   }
   QAction &Main_Window::save_a_copy_action()
   {
-    return *ui->action_Save_A_Copy;
+    return *ui->save_a_copy_action;
   }
   QAction &Main_Window::undo_action()
   {
-    return *ui->action_Undo;
+    return *ui->undo_action;
   }
   QAction &Main_Window::redo_action()
   {
-    return *ui->action_Redo;
+    return *ui->redo_action;
   }
   QAction &Main_Window::copy_action()
   {
-    return *ui->action_copy;
+    return *ui->copy_action;
   }
 
   void Main_Window::update_window_title(const QString &filename)
@@ -98,14 +98,14 @@ namespace editor {
     // it's a pitty that Qt Creator does not support
     // adding standard key sequences ... cf.
     // https://bugreports.qt.io/browse/QTBUG-1278
-    ui->action_Open->setShortcut(QKeySequence::Open);
-    ui->action_Save->setShortcut(QKeySequence::Save);
-    ui->action_Save_As->setShortcut(QKeySequence::SaveAs);
-    //ui->action_Save_A_Copy->setShortcut(...);
-    ui->action_Quit->setShortcut(QKeySequence::Quit);
-    ui->action_Undo->setShortcut(QKeySequence::Undo);
-    ui->action_Redo->setShortcut(QKeySequence::Redo);
-    ui->action_copy->setShortcut(QKeySequence::Copy);
+    ui->open_action->setShortcut(QKeySequence::Open);
+    ui->save_action->setShortcut(QKeySequence::Save);
+    ui->save_as_action->setShortcut(QKeySequence::SaveAs);
+    //ui->save_a_copy_action->setShortcut(...);
+    ui->quit_action->setShortcut(QKeySequence::Quit);
+    ui->undo_action->setShortcut(QKeySequence::Undo);
+    ui->redo_action->setShortcut(QKeySequence::Redo);
+    ui->copy_action->setShortcut(QKeySequence::Copy);
   }
 
   Main_Window::~Main_Window()
