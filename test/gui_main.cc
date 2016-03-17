55578cc51df2c16b2730c0e3e9da6c7b82e5fc66
Georg Sauthoff <mail@georg.so>
2016-03-17 11:07:21 +0100

    Forward some command line arguments to QTest
    For example: -platform offscreen

--- test/gui_main.cc
+++ test/gui_main.cc
@@ -25,19 +25,58 @@
 
 #include <QApplication>
 
-static char arg0[10] = "fake";
-static char *fake_argv[2] = {arg0};
-static int fake_argc = 1;
+#include <memory>
+#include <utility>
+#include <string.h>
+
+using namespace std;
+
+static pair<int, unique_ptr<char*[]> > create_qtest_argv(int ac, char **av)
+{
+  unique_ptr<char*[]> argv(new char*[ac]);
+  int argc = 0;
+  if (ac)
+    argv[argc++] = av[0];
+  for (int i = 1; i < ac; ++i) {
+    if (!strcmp(av[i], "-platform")) {
+      argv[argc++] = av[i];
+      ++i;
+      if (i < ac)
+        argv[argc++] = av[i];
+      break;
+    }
+  }
+  return make_pair(argc, std::move(argv));
+}
+
+static pair<int, unique_ptr<char*[]> > create_catch_argv(int ac, char **av)
+{
+  unique_ptr<char*[]> argv(new char*[ac]);
+  int argc = 0;
+  if (ac)
+    argv[argc++] = av[0];
+  for (int i = 1; i < ac; ++i) {
+    if (!strcmp(av[i], "-platform")) {
+      ++i;
+      continue;
+    }
+    argv[argc++] = av[i];
+  }
+  return make_pair(argc, std::move(argv));
+}
 
 int main(int argc, char **argv)
 {
+  auto qtest_argv = create_qtest_argv(argc, argv);
+  auto catch_argv = create_catch_argv(argc, argv);
+
   // without QApplication, widgets can segfault
   // on uninitialized singleton data ...
   // Also, QApplication must be constructed once, constructing it e.g.
   // in each test case yields segfaults ...
-  QApplication app(fake_argc, fake_argv);
+  QApplication app(qtest_argv.first, qtest_argv.second.get());
   app.setAttribute(Qt::AA_Use96Dpi, true);
   editor::File_Type::register_meta_type();
-  int result = Catch::Session().run( argc, argv );
+  int result = Catch::Session().run(catch_argv.first, catch_argv.second.get());
   return result;
 }
