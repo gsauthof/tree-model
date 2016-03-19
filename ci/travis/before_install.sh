791b7004a157307d5486904f1af5edd3be8bfa41
Georg Sauthoff <mail@georg.so>
2016-03-19 20:39:11 +0100

    travis: fix mapping

--- ci/travis/before_install.sh
+++ ci/travis/before_install.sh
@@ -3,12 +3,15 @@
 set -eux
 
 build=/var/tmp/build
+src="$PWD"/..
 
 mkdir -p $build
 
+# Travis' Ubuntu probably doesn't come with SELinux enabled, but
+# shouldn't hurt
 docker create --name cxx-devel \
-  -v $PWD:/srv/src:ro,Z \
-  -v $build:/srv/build:Z \
+  -v "$src":/srv/src:ro,Z \
+  -v "$build":/srv/build:Z \
   gsauthof/fedora-cxx-devel:23
 
 docker start cxx-devel
--- ci/travis/before_script.sh
+++ ci/travis/before_script.sh
@@ -3,12 +3,13 @@
 set -eux
 
 build=/var/tmp/build
+src="$PWD"/..
 
 docker stop cxx-devel
 
 docker create --name cxx-runtime \
-  -v $PWD/src:/srv/src:ro,Z \
-  -v $build:/srv/build:Z \
+  -v "$src":/srv/src:ro,Z \
+  -v "$build":/srv/build:Z \
   gsauthof/fedora-cxx-runtime:23
 
 docker start cxx-runtime
