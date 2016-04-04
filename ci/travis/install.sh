ef3dd9c1a20ef5a21ecb238034e5dc74662dc085
Georg Sauthoff <mail@georg.so>
2016-04-04 22:45:07 +0200

    Fix typo, missing env variable

--- ci/travis/install.sh
+++ ci/travis/install.sh
@@ -2,8 +2,12 @@
 
 set -eux
 
+: ${run_tag:=""}
+
 docker exec cxx-devel \
-  env CMAKE_BUILD_TYPE="$CMAKE_BUILD_TYPE" \
+  env \
+  CMAKE_BUILD_TYPE="$CMAKE_BUILD_TYPE" \
+  run_tag="$run_tag" \
   /srv/src/tree-model/ci/docker/build.sh \
   /srv/src/tree-model \
   /srv/build/tree-model
